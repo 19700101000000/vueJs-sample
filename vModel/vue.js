ea21cd89125a98ea7aff401ae78f8c49c334fe93
Asuka Yagi <yagi.asuka.0x06@gmail.com>
2018-10-18 19:40:08 +0900

    Fix v-model sample

--- vModel/vue.js
+++ vModel/vue.js
@@ -2,13 +2,14 @@ var app = new Vue({
 	el: '#app',
 	template: `
 		<div id="app">
-			<input v-for="i in 10" type="checkbox" :value="i + 'つ目'" v-model="checkbox">
+			<input v-for="i in num" type="checkbox" :value="i + 'つ目'" v-model="checkbox">
 			<div>
 				<template v-for="v in checkbox">{{ v }}</template>
 			</div>
 		</div>
 	`,
 	data: {
+		num: 10,
 		checkbox: [],
 	},
 })
